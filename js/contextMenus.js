477f0f3fdfa5ad45b75bf400236dc76fb8c04f45
r3nyou <renyou815@gmail.com>
2019-07-09 15:07:59 +0800

    popup login UI

--- js/contextMenus.js
+++ js/contextMenus.js
@@ -19,14 +19,23 @@ class ContextMenus {
 
     onClickHandler() {
         chrome.contextMenus.onClicked.addListener((info, tab) => {
-            console.log("item " + info.menuItemId + " was clicked");
-            console.log("info: " + JSON.stringify(info));
-            console.log("tab: " + JSON.stringify(tab));
+            //console.log("item " + info.menuItemId + " was clicked");
+            //console.log("info: " + JSON.stringify(info));
+            //console.log("tab: " + JSON.stringify(tab));
+            //chrome.runtime.sendMessage("hi");
+            this.sendInfo();
         });
-    }    
+    }
+
+    sendInfo() {
+        chrome.runtime.sendMessage('Hello', function(response){
+            //document.write(response);
+            //console.log("aaa" + response);
+        });
+    }
 }
 
-contextMenus = new ContextMenus();
-contextMenus.createParent();
-contextMenus.createChild();
-contextMenus.onClickHandler();
+//contextMenus = new ContextMenus();
+//contextMenus.createParent();
+//contextMenus.createChild();
+//contextMenus.onClickHandler();
