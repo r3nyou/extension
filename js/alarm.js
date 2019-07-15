b6b9a0a31e2271e776dfecb25913851e1c3d788d
IAGILY <61g4iagily@gmail.com>
2019-07-15 16:23:25 +0800

    update

--- js/alarm.js
+++ js/alarm.js
@@ -1,23 +1,37 @@
 class Alarm {
 
     constructor() {
+        this.timeStart = 0;
+        this.timeNow = 0;
+        this.timeRemaining = 0;
         this.alarmInfo = {
-            delayInMinutes: 1, 
+            delayInMinutes: 25,
         };
     }
 
     alarm() {
-        var ala = this.alarmInfo;
-        chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-            if(message == 'Hello'){
+        this.timeStart = 0;
+        this.timeNow = 0;
+        chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+            if (parseInt(message) <= 120 && parseInt(message) >= 10) {
+                sendResponse('Hello.');
+                this.alarmInfo.delayInMinutes = parseInt(message);
+            }
+        });
+        chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+            if (message == 'Hello') {
                 sendResponse('Hello from background.');
                 block.blockIt = true;
+                this.timeStart = Date.now() / 1000;
+                chrome.alarms.create('anAlarm', this.alarmInfo);
 
-                chrome.alarms.create('anAlarm', ala);
-
-                chrome.alarms.onAlarm.addListener(function() {
+                chrome.alarms.onAlarm.addListener(() => {
                     block.blockIt = false;
                 });
+                setInterval(() => {
+                    this.timeNow = Date.now() / 1000;
+                    this.timeRemaining = this.alarmInfo.delayInMinutes * 60 - (this.timeNow - this.timeStart);
+                }, 100);
             }
         });
     }
--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,38 @@
-// chrome.runtime.sendMessage('Hello', function(response){
-//   document.getElementById('mes').innerHTML = response;
-// });
-
-var btn1Obj = document.getElementById("btn_start"); 
-btn1Obj.addEventListener("click",function(){
-  chrome.runtime.sendMessage('Hello', function(response){
+var bgpage = chrome.extension.getBackgroundPage();
+var btnTimeDecrease = document.getElementById("time_--");
+var btnTimeIncrease = document.getElementById("time_++");
+var btnStart = document.getElementById("btn_start");
+document.getElementById('forDelayInMinutes').innerHTML = bgpage.alarm.alarmInfo.delayInMinutes;
+btnTimeDecrease.addEventListener("click", () => {
+  document.getElementById('forDelayInMinutes').innerHTML = (parseInt(document.getElementById('forDelayInMinutes').innerHTML) - 5) <= 10 ? 10 : (parseInt(document.getElementById('forDelayInMinutes').innerHTML) - 5);
+  chrome.runtime.sendMessage(document.getElementById('forDelayInMinutes').innerHTML, (response) => {
     document.getElementById('mes').innerHTML = response;
+
   });
-},false); 
+}, false);
+btnTimeIncrease.addEventListener("click", () => {
+  document.getElementById('forDelayInMinutes').innerHTML = (parseInt(document.getElementById('forDelayInMinutes').innerHTML) + 5) >= 120 ? 120 : (parseInt(document.getElementById('forDelayInMinutes').innerHTML) + 5);
+  chrome.runtime.sendMessage(document.getElementById('forDelayInMinutes').innerHTML, (response) => {
+    document.getElementById('mes').innerHTML = response;
+
+  });
+}, false);
+btnStart.addEventListener("click", () => {
+  chrome.runtime.sendMessage('Hello', (response) => {
+    document.getElementById('mes').innerHTML = response;
+
+  });
+}, false);
+
+setInterval(() => {
+  if (bgpage.block.blockIt) {
+    var h = Math.floor(bgpage.alarm.timeRemaining / 3600);
+    var m = Math.floor((bgpage.alarm.timeRemaining % 3600) / 60);
+    var s = Math.floor(bgpage.alarm.timeRemaining % 60);
+    h = h >= 1 ? h : ('0');
+    m = m >= 1 ? m : ('0');
+    m = m >= 10 ? m : ('0' + m);
+    s = s >= 10 ? s : ('0' + s);
+    document.getElementById('mes').innerHTML = h + ":" + m + ":" + s;
+  }
+}, 100);
